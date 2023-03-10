36d681a4e73fe130abe15f76aaa2ee6c37bb1a58
Miracleyoo <miracleyoo@163.com>
2023-03-09 18:44:00 -0800

    f

--- scripts/dvs_V1_train_resnet.sh
+++ scripts/dvs_V1_train_resnet.sh
@@ -5,6 +5,6 @@ DATASET=$(cat $TRAIN_CONFIGS | shyaml get-value ARGS.dataset)
 TAB=$(cat $TRAIN_CONFIGS | shyaml get-value ARGS.tab)
 
 # CUDA_VISIBLE_DEVICES=${GPUS} python -u -m event_romp.train --configs_yml=${TRAIN_CONFIGS}
-CUDA_VISIBLE_DEVICES=${GPUS} torchrun -u -m event_romp.train --configs_yml=${TRAIN_CONFIGS}
+CUDA_VISIBLE_DEVICES=${GPUS} torchrun -m event_romp.train --configs_yml=${TRAIN_CONFIGS}
 
 #CUDA_VISIBLE_DEVICES=${GPUS} nohup python -u -m romp.train --configs_yml=${TRAIN_CONFIGS} > 'log/'${TAB}'_'${DATASET}'_g'${GPUS}.log 2>&1 &
