4842ffb03793a17a7ca2efc7b478a5c26fc7e061
Miracleyoo <miracleyoo@163.com>
2023-03-16 01:50:51 -0700

    f

--- event_romp/lib/utils/train_utils.py
+++ event_romp/lib/utils/train_utils.py
@@ -31,7 +31,6 @@ def copy_state_dict(cur_state_dict, pre_state_dict, drop_prefix='', fix_loaded=F
         unfreeze_heads = unfreeze_head_dict[backbone]
     
     for k in cur_state_dict.keys():
-        print('k:', k, type(k))
         if partial_freeze and any([k.lstrip('module.').startswith(uh) for uh in unfreeze_heads]):
             continue
         
